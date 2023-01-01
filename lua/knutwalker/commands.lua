c4b2cd7a16f8b6e37ae91c7a12af67f9b76ab42a
Paul Horn <dev@knutwalker.engineer>
2023-01-01 03:53:34 +0100

    Check if we need to reload the file when it changed outside of nvim

--- lua/knutwalker/commands.lua
+++ lua/knutwalker/commands.lua
@@ -1,3 +1,6 @@
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
+
 -- Highlight on yank
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
