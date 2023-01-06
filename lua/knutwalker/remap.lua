40e2bea2e46aa1c304b446822a8e710a3308e555
Paul Horn <dev@knutwalker.engineer>
2023-01-06 19:37:46 +0100

    Move 'paste from "_' to a usable keybind

--- lua/knutwalker/remap.lua
+++ lua/knutwalker/remap.lua
@@ -23,7 +23,7 @@ bind("n", "n", "nzzzv", { desc = "Next search result" })
 bind("n", "N", "Nzzzv", { desc = "Previous search result" })
 
 -- paste with space v without losing the copied content
-bind("x", "<leader>p", '"_dp', { desc = "[P]aste without losing copied content" })
+bind({ "n", "v", "x" }, "<leader>v", '"_dp', { desc = "Paste without losing copied content" })
 
 -- yank and paste to and from the system clipboard
 bind("n", "<leader>y", '"+y', { desc = "[Y]ank to system clipboard" })
