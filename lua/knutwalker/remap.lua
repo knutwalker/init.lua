9c205ff13c593743885f7d69711c40ce2e136acf
Paul Horn <dev@knutwalker.engineer>
2023-01-08 00:48:04 +0100

    Update lazygit and gitdrive after lazy.nvim update

--- lua/knutwalker/remap.lua
+++ lua/knutwalker/remap.lua
@@ -124,16 +124,10 @@ bind(
 )
 
 -- lazygit integration
-bind("n", "<leader>gl", function(cwd)
-	require("lazy.util").open_cmd({ "lazygit" }, {
-		cwd = cwd,
-		terminal = true,
-		close_on_exit = true,
-		enter = true,
-		float = {
-			size = { width = 0.95, height = 0.95 },
-			margin = { top = 0, right = 0, bottom = 0, left = 0 },
-		},
+bind("n", "<leader>gl", function()
+	require("lazy.util").float_term({ "lazygit" }, {
+		size = { width = 0.95, height = 0.95 },
+		margin = { top = 0, right = 0, bottom = 0, left = 0 },
 	})
 end, { desc = "Open [l]azygit" })
 bind("n", "<leader>ly", "<leader>gl", { desc = "Open [l]az[y]git", remap = true })
@@ -144,15 +138,10 @@ local function git_drive(args)
 	for _, v in ipairs(args or {}) do
 		table.insert(cmd, v)
 	end
-	require("lazy.util").open_cmd(cmd, {
-		terminal = true,
-		close_on_exit = true,
-		enter = true,
-		float = {
-			win_opts = { border = "rounded" },
-			size = { width = 0.75, height = 0.75 },
-			margin = { top = 2, right = 2, bottom = 2, left = 2 },
-		},
+	require("lazy.util").float_term(cmd, {
+		border = "rounded",
+		size = { width = 0.75, height = 0.75 },
+		margin = { top = 2, right = 2, bottom = 2, left = 2 },
 	})
 end
 
