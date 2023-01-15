636bbc852770d4cfa06b2bef95105195c8388614
Paul Horn <dev@knutwalker.engineer>
2023-01-15 15:44:52 +0100

    Move init code to init function

--- lua/knutwalker/plugins/neotree.lua
+++ lua/knutwalker/plugins/neotree.lua
@@ -1,12 +1,13 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v2.x",
 	cmd = "Neotree",
 	dependencies = { "MunifTanjim/nui.nvim" },
 	keys = require("knutwalker.keymap").neotree,
+	init = function()
+		-- Unless you are still migrating, remove the deprecated commands from v1.x
+		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+	end,
 	opts = {
 		close_if_last_window = true,
 		sort_case_insensitive = true,
