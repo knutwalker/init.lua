e91641636855b318cb560fa76c56c4edc7418207
Paul Horn <dev@knutwalker.engineer>
2023-01-18 13:58:28 +0100

    Disable global status line

--- lua/knutwalker/plugins/lualine.lua
+++ lua/knutwalker/plugins/lualine.lua
@@ -13,7 +13,7 @@ return {
 			},
 			ignore_focus = {},
 			always_divide_middle = true,
-			globalstatus = true,
+			globalstatus = false,
 			refresh = {
 				statusline = 2500,
 				tabline = 2500,
