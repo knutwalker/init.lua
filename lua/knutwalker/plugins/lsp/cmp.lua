f4fcb41cb0b87bdd3d06b9c7f0772d3c3fa35e34
Paul Horn <dev@knutwalker.engineer>
2023-02-27 13:06:00 +0100

    Show cmp by default

--- lua/knutwalker/plugins/lsp/cmp.lua
+++ lua/knutwalker/plugins/lsp/cmp.lua
@@ -108,7 +108,6 @@ function M.setup(lsp)
 		preselect = "none",
 		-- matches what is configured in set.lua
 		completion = {
-			autocomplete = false,
 			completeopt = "menu,menuone,noinsert,noselect,preview",
 		},
 		-- configure the LSP as expansion engine, this should be set by LSP-zero as well
