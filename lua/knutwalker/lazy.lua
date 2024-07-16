62e43d67145d3203590ba9e41aa882b4355e23bb
Paul Horn <dev@knutwalker.engineer>
2024-07-17 00:24:42 +0200

    Update plugins

--- lua/knutwalker/lazy.lua
+++ lua/knutwalker/lazy.lua
@@ -1,52 +1,56 @@
 -- bootstrap from github
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"git@github.com:folke/lazy.nvim.git",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--single-branch",
+        "git@github.com:folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 -- load lazy
 require("lazy").setup("knutwalker.plugins", {
-	defaults = { lazy = false },
-	install = { colorscheme = { require("knutwalker.colors").default_color_scheme } },
-	checker = {
-		-- do not automatically check for plugin updates
-		enabled = false,
-	},
-	change_detection = {
-		-- Don't notify when changes are found
-		notify = false,
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"2html_plugin",
-				"getscript",
-				"getscriptPlugin",
-				"logipat",
-				"netrw",
-				"netrwPlugin",
-				"netrwSettings",
-				"netrwFileHandlers",
-				"rrhelper",
-				"spellfile_plugin",
-				"vimball",
-				"vimballPlugin",
-				"tutor",
-				"rplugin",
-				"synmenu",
-				"optwin",
-				"compiler",
-				"bugreport",
-			},
-		},
-	},
+    defaults = { lazy = false },
+    install = { colorscheme = { require("knutwalker.colors").default_color_scheme } },
+    checker = {
+        -- do not automatically check for plugin updates
+        enabled = false,
+    },
+    change_detection = {
+        -- Don't notify when changes are found
+        notify = false,
+    },
+    rocks = {
+        enabled = false,
+        hererocks = false,
+    },
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "2html_plugin",
+                "getscript",
+                "getscriptPlugin",
+                "logipat",
+                "netrw",
+                "netrwPlugin",
+                "netrwSettings",
+                "netrwFileHandlers",
+                "rrhelper",
+                "spellfile_plugin",
+                "vimball",
+                "vimballPlugin",
+                "tutor",
+                "rplugin",
+                "synmenu",
+                "optwin",
+                "compiler",
+                "bugreport",
+            },
+        },
+    },
 })
 vim.keymap.set("n", "<leader>lz", "<cmd>:Lazy<cr>")
--- lua/knutwalker/plugins/init.lua
+++ lua/knutwalker/plugins/init.lua
@@ -242,6 +242,7 @@ return {
 
     {
         "supermaven-inc/supermaven-nvim",
+        commit = 'df3ecf7',
         event = "InsertEnter",
         config = function()
             require("supermaven-nvim").setup({
