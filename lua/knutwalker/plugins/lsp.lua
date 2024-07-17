038c3b8ce30f9ae12f403d0f66f4686eac7654fb
Paul Horn <dev@knutwalker.engineer>
2024-07-17 11:03:39 +0200

    Configure swift lsp

--- lua/knutwalker/plugins/lsp.lua
+++ lua/knutwalker/plugins/lsp.lua
@@ -1,117 +1,138 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v3.x",
-	event = "BufReadPre",
-	dependencies = {
-		-- LSP Support
-		"neovim/nvim-lspconfig",
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    event = "BufReadPre",
+    dependencies = {
+        -- LSP Support
+        "neovim/nvim-lspconfig",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
 
-		-- Improved Rust support
-		{ "simrat39/rust-tools.nvim" },
+        -- Improved Rust support
+        { "simrat39/rust-tools.nvim" },
 
-		-- Java LSP support
-		{ "mfussenegger/nvim-jdtls" },
+        -- Java LSP support
+        { "mfussenegger/nvim-jdtls" },
 
-		-- Formatting
-		"nvimtools/none-ls.nvim",
-		"jayp0521/mason-null-ls.nvim",
+        -- Formatting
+        "nvimtools/none-ls.nvim",
+        "jayp0521/mason-null-ls.nvim",
 
-		-- Autocompletion
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
-		"petertriho/cmp-git",
-		"hrsh7th/cmp-emoji",
-		"saadparwaiz1/cmp_luasnip",
+        -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lua",
+        "petertriho/cmp-git",
+        "hrsh7th/cmp-emoji",
+        "saadparwaiz1/cmp_luasnip",
 
-		-- Snippets
-		"L3MON4D3/LuaSnip",
-		"rafamadriz/friendly-snippets",
+        -- Snippets
+        "L3MON4D3/LuaSnip",
+        "rafamadriz/friendly-snippets",
 
-		-- Progress Report
-		{ "j-hui/fidget.nvim", tag = "legacy" },
+        -- Progress Report
+        { "j-hui/fidget.nvim",                                 tag = "legacy" },
 
-		-- LSP kind symbols for completion items
-		"onsails/lspkind-nvim",
+        -- LSP kind symbols for completion items
+        "onsails/lspkind-nvim",
 
-		-- Multiline diagnostics
-		{ url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
+        -- Multiline diagnostics
+        { url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
 
-		-- File location
-		{ "SmiteshP/nvim-navic", lazy = true },
+        -- File location
+        { "SmiteshP/nvim-navic",                               lazy = true },
 
-		-- LSP Signature
-		{ "ray-x/lsp_signature.nvim", config = true },
+        -- LSP Signature
+        { "ray-x/lsp_signature.nvim",                          config = true },
 
-		-- Incremental renaming
-		{ "smjonas/inc-rename.nvim", config = true },
-	},
-	config = function()
-		local lsp = require("lsp-zero")
+        -- Incremental renaming
+        { "smjonas/inc-rename.nvim",                           config = true },
+    },
+    config = function()
+        local lsp = require("lsp-zero")
 
-		require("knutwalker.plugins.lsp.cmp").setup(lsp)
-		require("knutwalker.plugins.lsp.diagnostics").setup_before_lsp()
-		lsp.on_attach(require("knutwalker.plugins.lsp.attach").setup())
+        require("knutwalker.plugins.lsp.cmp").setup(lsp)
+        require("knutwalker.plugins.lsp.diagnostics").setup_before_lsp()
+        lsp.on_attach(require("knutwalker.plugins.lsp.attach").setup())
 
-		lsp.set_preferences({
-			-- use simple letters instead of fancy icons
-			sign_icons = {
-				error = "E",
-				warn = "W",
-				hint = "H",
-				info = "I",
-			},
-			-- Don't setup default keymap, we configure our own
-			set_lsp_keymaps = false,
-			-- Don't configure diagnostics, we do this ourselves
-			configure_diagnostics = false,
-		})
+        lsp.set_preferences({
+            -- use simple letters instead of fancy icons
+            sign_icons = {
+                error = "E",
+                warn = "W",
+                hint = "H",
+                info = "I",
+            },
+            -- Don't setup default keymap, we configure our own
+            set_lsp_keymaps = false,
+            -- Don't configure diagnostics, we do this ourselves
+            configure_diagnostics = false,
+        })
 
-		-- configure yaml language server
-		lsp.configure("yamlls", {
-			settings = {
-				yaml = { keyOrdering = false },
-			},
-		})
+        -- configure yaml language server
+        lsp.configure("yamlls", {
+            settings = {
+                yaml = { keyOrdering = false },
+            },
+        })
 
-		lsp.configure("cypher_ls", {})
+        lsp.configure("cypher_ls", {})
 
-		-- save configs for RA/JDT and null-ls since we set those up manually
-		local rust_lsp_opts = lsp.build_options("rust_analyzer", {})
-		local jdtls_lsp_opts = lsp.build_options("jdtls", {})
+        -- save configs for RA/JDT and null-ls since we set those up manually
+        local rust_lsp_opts = lsp.build_options("rust_analyzer", {})
+        local jdtls_lsp_opts = lsp.build_options("jdtls", {})
 
-		lsp.setup()
-		require("mason").setup({})
-		require("mason-null-ls").setup({})
-		require("mason-lspconfig").setup({
-			handlers = {
-				lsp.default_setup,
-				lua_ls = function()
-					local lua_opts = lsp.nvim_lua_ls()
-					require("lspconfig").lua_ls.setup(lua_opts)
-				end,
-			},
-		})
+        lsp.setup()
 
-		-- Setup LSP diagnostics
-		require("knutwalker.plugins.lsp.diagnostics").setup_after_lsp()
+        local function mason_lspconfig_setup()
+            local lspconfig = require("lspconfig")
 
-		-- Configure rust-tools
-		require("knutwalker.plugins.lsp.rust").setup(rust_lsp_opts)
+            local function set_lspconfig(server, lsp_options)
+                lspconfig[server].setup(lsp_options)
+            end
 
-		-- Configure JDT ls
-		require("knutwalker.plugins.lsp.java").setup(jdtls_lsp_opts)
+            set_lspconfig("sourcekit", {
+                capabilities = {
+                    workspace = {
+                        didChangeWatchedFiles = {
+                            dynamicRegistration = true,
+                        },
+                    },
+                },
+            })
 
-		-- Configure Formatter
+            return {
+                lsp.default_setup,
+                lua_ls = function()
+                    local lua_opts = lsp.nvim_lua_ls()
+                    set_lspconfig("lua_ls", lua_opts)
+                end,
+            }
+        end
 
-		-- Configure git source
-		require("cmp_git").setup()
+        require("mason").setup({})
+        require("mason-null-ls").setup({})
+        require("mason-lspconfig").setup({
+            handlers = mason_lspconfig_setup(),
+        })
 
-		-- enable progress report
-		require("fidget").setup()
-	end,
+        -- Setup LSP diagnostics
+        require("knutwalker.plugins.lsp.diagnostics").setup_after_lsp()
+
+        -- Configure rust-tools
+        require("knutwalker.plugins.lsp.rust").setup(rust_lsp_opts)
+
+        -- Configure JDT ls
+        require("knutwalker.plugins.lsp.java").setup(jdtls_lsp_opts)
+
+        -- Configure Formatter
+
+        -- Configure git source
+        require("cmp_git").setup()
+
+        -- enable progress report
+        require("fidget").setup()
+    end,
 }
