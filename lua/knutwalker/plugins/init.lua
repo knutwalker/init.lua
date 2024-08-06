ff09a3366e0b814d32b110e691bd94193161ec0e
Paul Horn <paul.horn@neo4j.com>
2024-08-06 12:03:35 +0200

    Add markview plugin

--- lua/knutwalker/plugins/init.lua
+++ lua/knutwalker/plugins/init.lua
@@ -204,4 +204,21 @@ return {
             auto_session_suppress_dirs = { '/', '~/', '~/Downloads', '~/dev/tmp/' },
         },
     },
+
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false, -- Recommended
+        opts = {
+            modes = { "n", "i", "no", "c" },
+            hybrid_modes = { "i" },
+
+            -- This is nice to have
+            callbacks = {
+                on_enable = function(_, win)
+                    vim.wo[win].conceallevel = 2;
+                    vim.wo[win].conecalcursor = "nc";
+                end
+            }
+        }
+    }
 }
